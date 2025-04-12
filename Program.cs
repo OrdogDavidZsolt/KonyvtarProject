a5653fb934c734951819a4d6357b2c31bbc9ceb4
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-04-12 20:20:30 +0200

    +Added Swagger +Added Serilog -Removed default generated program

--- Program.cs
+++ Program.cs
@@ -1,22 +1,33 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSerilog(
+    options => options
+        .MinimumLevel.Information()
+        .WriteTo.Console()
+);
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSingleton< , >();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
